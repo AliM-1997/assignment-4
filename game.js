d60859375cb770fb1031ea61acc2983043793206
AliM-1997 <aliimortada797@gmail.com>
2024-06-30 16:27:10 +0300

    getting the game sequence

--- game.js
+++ game.js
@@ -12,3 +12,31 @@ document.addEventListener('keypress', () => {
         started = true
     }
 })
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    document.querySelector("#level-title").textContent = `Level ${level}`;
+
+    const randomNumber = Math.floor(Math.random() * 4);
+    const randomChosenColor = buttonColors[randomNumber];
+    
+    gamePattern.push(randomChosenColor);
+    const btn = document.querySelector(`#${randomChosenColor}`);
+    btn.classList.add('pressed');
+
+}
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    document.querySelector("#level-title").textContent = `Level ${level}`;
+
+    const randomNumber = Math.floor(Math.random() * 4);
+    const randomChosenColor = buttonColors[randomNumber];
+    
+    gamePattern.push(randomChosenColor);
+    const btn = document.querySelector(`#${randomChosenColor}`);
+    btn.classList.add('pressed');
+}
+
+
